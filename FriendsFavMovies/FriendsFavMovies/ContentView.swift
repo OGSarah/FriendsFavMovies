d9d08b1b33437c1ac23643df1f5669f47a01e7a4
Sarah Clark <sarahireneclark@gmail.com>
2025-08-21 19:29:01 -0500

    Initial commit of project

---
+++ FriendsFavMovies/FriendsFavMovies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendsFavMovies
+//
+//  Created by Sarah Clark on 8/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendsFavMovies/FriendsFavMovies/FriendsFavMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavMoviesApp.swift
+//  FriendsFavMovies
+//
+//  Created by Sarah Clark on 8/21/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
